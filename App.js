9a4efd4b1a63dd2d402007413c8747fd7562ede0
Surachet Sungkhapan <surachetsungkhapan@gmail.com>
2021-07-02 10:49:58 +0700

    Update App.js
    แก้ไขข้อความใน Code

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test React Native</Text>
+      <Text>Test React Native ทดสอบ React Native เบื้องต้น</Text>
       <StatusBar style="auto" />
     </View>
   );
